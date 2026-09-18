ce289c43cdb3935a5419aac1ba7f9856c0c20a47
OhRidgeInAll <hollandg@sheridancollege.ca>
2026-09-18 13:08:51 -0400

    Demo Git Commit showing local commit functionality (base project)

---
+++ src/student/Student.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author smile
+ */
+public class Student {
+    String lastName;
+    int id;
+    double finalGrade;
+    
+        public Student(String last, int stid, double finalG){
+            this.lastName = last;
+            this.id = stid;
+            this.finalGrade = finalG;
+    
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getFinalGrade() {
+        return finalGrade;
+    }
+
+    public void setFinalGrade(double finalGrade) {
+        this.finalGrade = finalGrade;
+    }
+    
+        
+        
+    
+}
+
+
---
+++ src/student/StudentList.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+import java.util.Scanner;
+
+/**
+ *
+ * @author smile
+ */
+public class StudentList {
+    public static void main(String[] args) { 
+        Scanner scan = new Scanner(System.in);
+        
+        Student s1 = new Student("Karin", 3, 85.0);
+        Student s2 = new Student("Smith", 1, 100.0);
+        Student s3 = new Student("Luffy", 100, 50.0);
+        
+        Student[] studentList = {s1, s2, s3};
+        
+        for(Student student : studentList){
+            System.out.println(student.getLastName());
+        }
+        
+        System.out.println("Enter Student to check grade of: \n");
+        String studentInput = scan.nextLine();
+        boolean studentFound = false;
+        
+        for(Student student : studentList){
+            if (studentFound == true){
+                break;
+            }
+            if(student.getLastName().equals(studentInput)){
+                System.out.println(student.finalGrade);
+                studentFound = true;
+            }
+        }
+        if(studentFound == false){
+            System.out.println("Student not found");
+        }
+    }
+    
+}
